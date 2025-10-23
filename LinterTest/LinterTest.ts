70f88cd9875e0b7b0c9e0c76c5b322791214d5a2
0maumau0 <strubemaurice@gmail.com>
2025-10-23 15:48:03 +0200

    solved LinterTest

--- LinterTest/LinterTest.ts
+++ LinterTest/LinterTest.ts
@@ -1,4 +1,4 @@
-namespace linterTest {
+namespace LinterTest {
 
   interface Info {
     text: string,
@@ -10,15 +10,15 @@ namespace linterTest {
   
   
   const info: Info = { text: "G`udetmvhsgBncd1 ", key: KEY.POS };
-  console.log(DeCrypt(info.text, info.key));
+  console.log(deCrypt(info.text, info.key));
   
   
   
 
-  function DeCrypt(_text: string, _Key: number) {
+  function deCrypt(_text: string, _key: number):string {
     let result: string = "";
-    for (let i = 0; i < _text.length; i++)
-      result += String.fromCharCode(_text.charCodeAt(i) + _Key);
+    for (let i:number = 0; i < _text.length; i++)
+      result += String.fromCharCode(_text.charCodeAt(i) + _key);
 
     return result;
     //text length isnt rigth!
