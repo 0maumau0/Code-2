ef31489ce9aa63466e60c2dd949f02fc22fd3290
0maumau0 <strubemaurice@gmail.com>
2025-10-23 17:34:00 +0200

    moving chassis

---
+++ Droid/Droid.js
@@ -0,0 +1,14 @@
+export var Droid;
+(function (Droid) {
+    function getCommand() {
+        const command = {
+            module: "Chassis",
+            method: "move",
+            data: "left",
+        };
+        console.log(command);
+        return command;
+    }
+    Droid.getCommand = getCommand;
+})(Droid || (Droid = {}));
+//# sourceMappingURL=Droid.js.map
--- LinterTest/LinterTest.js
+++ LinterTest/LinterTest.js
@@ -1,19 +1,19 @@
 "use strict";
-var linterTest;
-(function (linterTest) {
+var LinterTest;
+(function (LinterTest) {
     let KEY;
     (function (KEY) {
         KEY[KEY["POS"] = 1] = "POS";
         KEY[KEY["NEG"] = -1] = "NEG";
     })(KEY || (KEY = {}));
     const info = { text: "G`udetmvhsgBncd1 ", key: KEY.POS };
-    console.log(DeCrypt(info.text, info.key));
-    function DeCrypt(text, _Key) {
+    console.log(deCrypt(info.text, info.key));
+    function deCrypt(_text, _key) {
         let result = "";
-        for (let i = 0; i < text.length; i++)
-            result += String.fromCharCode(text.charCodeAt(i) + _Key);
+        for (let i = 0; i < _text.length; i++)
+            result += String.fromCharCode(_text.charCodeAt(i) + _key);
         return result;
         //text length isnt rigth!
     }
-})(linterTest || (linterTest = {}));
+})(LinterTest || (LinterTest = {}));
 //# sourceMappingURL=LinterTest.js.map
