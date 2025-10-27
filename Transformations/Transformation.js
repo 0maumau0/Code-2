1fd0e3e7df36df409728a5226c7782a383d43ad5
0maumau0 <strubemaurice@gmail.com>
2025-10-27 16:39:42 +0100

    color transforming coordinatesystem

--- Transformations/Transformation.js
+++ Transformations/Transformation.js
@@ -1,40 +1,48 @@
 "use strict";
 var Transformations;
 (function (Transformations) {
+    const colors = ["lightyellow", "green", "red", "blue"];
     window.addEventListener("load", hndlLoad);
     function hndlLoad() {
         const canvas = document.querySelector("canvas");
         const crc2 = canvas.getContext("2d");
         canvas.height = 600;
         canvas.width = 600;
-        crc2.fillStyle = "lightyellow";
+        crc2.fillStyle = colors[0];
         crc2.fillRect(0, 0, canvas.height, canvas.width);
-        drawCoordinateSystem(crc2);
+        drawCoordinateSystem(crc2, colors[1]);
+        crc2.save();
+        crc2.rotate((60 * Math.PI) / 180);
+        drawCoordinateSystem(crc2, colors[4]);
+        // crc2.restore();
+        crc2.save();
+        crc2.scale(3, 1.7);
+        drawCoordinateSystem(crc2, colors[3]);
+        // crc2.restore();
+        crc2.save();
+        crc2.translate(70, -100);
+        drawCoordinateSystem(crc2, colors[2]);
+        // crc2.restore();
     }
-    function drawCoordinateSystem(_crc2) {
+    function drawCoordinateSystem(_crc2, _color) {
         _crc2.beginPath();
         _crc2.moveTo(0, 0);
         _crc2.lineTo(0, 200);
-        _crc2.closePath();
+        _crc2.strokeStyle = _color;
         _crc2.stroke();
         _crc2.beginPath();
         _crc2.moveTo(0, 0);
         _crc2.lineTo(200, 0);
-        _crc2.closePath();
+        _crc2.strokeStyle = _color;
         _crc2.stroke();
-        for (let i = 0; i <= 200; i++) {
-            if (i % 10 == 0) {
-                _crc2.beginPath();
-                _crc2.moveTo(0, 0);
-                _crc2.lineTo(i, -10);
-                _crc2.lineTo(i, +10);
-                _crc2.stroke();
-                _crc2.beginPath();
-                _crc2.moveTo(0, 0);
-                _crc2.lineTo(-10, i);
-                _crc2.lineTo(10, i);
-                _crc2.stroke();
-            }
+        for (let i = 0; i <= 200; i += 10) {
+            _crc2.beginPath();
+            _crc2.moveTo(i, -10);
+            _crc2.lineTo(i, +10);
+            _crc2.moveTo(-10, i);
+            _crc2.lineTo(10, i);
+            _crc2.strokeStyle = _color;
+            _crc2.stroke();
         }
     }
 })(Transformations || (Transformations = {}));
--- Transformations/Transformation.ts
+++ Transformations/Transformation.ts
@@ -3,6 +3,8 @@ namespace Transformations {
 
     type Vector2 = { x: number, y: number };
 
+     const colors:string [] = ["lightyellow","green", "red","blue"]
+
 
     window.addEventListener("load", hndlLoad)
 
@@ -11,44 +13,56 @@ namespace Transformations {
         const canvas: HTMLCanvasElement = document.querySelector("canvas")!;
         const crc2: CanvasRenderingContext2D = canvas.getContext("2d")!;
 
+        
+
         canvas.height = 600;
         canvas.width = 600;
-        crc2.fillStyle = "lightyellow"
-        crc2.fillRect(0,0,canvas.height,canvas.width);
+        crc2.fillStyle = colors[0]
+        crc2.fillRect(0, 0, canvas.height, canvas.width);
+
+        drawCoordinateSystem(crc2,colors[1]);
+       
+
+        crc2.save();
+        crc2.rotate((60*Math.PI)/180);
+        drawCoordinateSystem(crc2,colors[4]);
+        // crc2.restore();
+
+        crc2.save();
+        crc2.scale(3,1.7);
+        drawCoordinateSystem(crc2,colors[3]);
+        // crc2.restore();
+
+        crc2.save();
+        crc2.translate(70, -100);
+        drawCoordinateSystem(crc2,colors[2]);
+        // crc2.restore();
         
-      drawCoordinateSystem(crc2);
     }
 
-    function drawCoordinateSystem(_crc2:CanvasRenderingContext2D):void{
-
+    function drawCoordinateSystem(_crc2: CanvasRenderingContext2D,_color:string): void {
         _crc2.beginPath();
-        _crc2.moveTo(0,0);
-        _crc2.lineTo(0,200);
-        _crc2.closePath();
+        _crc2.moveTo(0, 0);
+        _crc2.lineTo(0, 200);
+        _crc2.strokeStyle = _color;
         _crc2.stroke();
 
         _crc2.beginPath();
-        _crc2.moveTo(0,0);
-        _crc2.lineTo(200,0);
-        _crc2.closePath();
+        _crc2.moveTo(0, 0);
+        _crc2.lineTo(200, 0);
+         _crc2.strokeStyle = _color;
         _crc2.stroke();
 
-        for (let i :number = 0; i <=200; i++){
-       
-            if (i %10 ==0 ){
-                _crc2.beginPath();
-                _crc2.moveTo(0,0);
-                _crc2.lineTo(i,-10);
-                _crc2.lineTo(i,+10)
-                _crc2.stroke();
+        for (let i: number = 0; i <= 200; i += 10) {
+            _crc2.beginPath();
+            
+            _crc2.moveTo(i, -10);
+            _crc2.lineTo(i, +10);
 
-                _crc2.beginPath();
-                _crc2.moveTo(0,0);
-                _crc2.lineTo(-10,i);
-                _crc2.lineTo(10,i)
-                _crc2.stroke();
-            }
-
+            _crc2.moveTo(-10, i);
+            _crc2.lineTo(10, i)
+             _crc2.strokeStyle = _color;
+            _crc2.stroke();
         }
     }
 }
