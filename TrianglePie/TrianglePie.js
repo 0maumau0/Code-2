a33b4cb33322e5a0e327490566cf891f8a263f99
0maumau0 <strubemaurice@gmail.com>
2025-10-28 00:18:17 +0100

    working Triangle function

--- TrianglePie/TrianglePie.js
+++ TrianglePie/TrianglePie.js
@@ -3,8 +3,10 @@ var TrianglePie;
 (function (TrianglePie) {
     window.addEventListener("load", hndlLoad);
     const pieces = 10;
-    const radius = 30;
+    const radius = 150;
     const centralPoint = { x: 300, y: 300 };
+    const point2 = { x: 0, y: 0 };
+    const point3 = { x: 0, y: 0 };
     function hndlLoad() {
         const canvas = document.querySelector("canvas");
         const crc2 = canvas.getContext("2d");
@@ -12,13 +14,35 @@ var TrianglePie;
         canvas.width = window.innerWidth;
         crc2.fillStyle = "blue";
         crc2.fillRect(0, 0, window.innerWidth, window.innerHeight);
-        drawtrianglePie(crc2, pieces);
+        drawtrianglePie(crc2, pieces, radius);
     }
-    function drawtrianglePie(_crc2, _pieces) {
+    function drawtrianglePie(_crc2, _pieces, _radius) {
         const rotationAngle = (2 * Math.PI) / _pieces;
         for (let i = 0; i < _pieces; i++) {
+            const startPoint2 = { x: 0, y: 0 - _radius };
+            const startPoint3 = { x: 0, y: 0 - _radius };
+            const rotatedPoint2 = rotatePoint(rotationAngle * i, startPoint2);
+            const rotatedPoint3 = rotatePoint(rotationAngle + rotationAngle * i, startPoint3);
+            point2.x = rotatedPoint2.x + centralPoint.x;
+            point2.y = rotatedPoint2.y + centralPoint.y;
+            point3.x = rotatedPoint3.x + centralPoint.x;
+            point3.y = rotatedPoint3.y + centralPoint.y;
+            drawtriangle(_crc2, centralPoint, point2, point3);
+        }
+        function rotatePoint(_angle, _point) {
+            let rotated = { x: 0, y: 0 };
+            rotated.x = (Math.cos(_angle) * _point.x) - (Math.sin(_angle) * _point.y);
+            rotated.y = (Math.sin(_angle) * _point.x) + (Math.cos(_angle) * _point.y);
+            return rotated;
         }
     }
-    // christians code and finishe circle of triangles
+    function drawtriangle(_crc2, _centralpoint, _point2, _point3) {
+        _crc2.beginPath();
+        _crc2.moveTo(_centralpoint.x, _centralpoint.y);
+        _crc2.lineTo(_point2.x, _point2.y);
+        _crc2.lineTo(_point3.x, _point3.y);
+        _crc2.closePath();
+        _crc2.stroke();
+    }
 })(TrianglePie || (TrianglePie = {}));
 //# sourceMappingURL=TrianglePie.js.map
--- TrianglePie/TrianglePie.ts
+++ TrianglePie/TrianglePie.ts
@@ -8,7 +8,7 @@ namespace TrianglePie {
     }
 
     const pieces: number = 10;
-    const radius: number = 30;
+    const radius: number = 150;
     const centralPoint: Vector2 = { x: 300, y: 300 };
     const point2: Vector2 = {x:0,y:0};
     const point3: Vector2 = {x:0,y:0};
@@ -35,14 +35,16 @@ namespace TrianglePie {
             const startPoint2: Vector2 = { x: 0, y: 0 - _radius };
             const startPoint3: Vector2 = { x: 0, y: 0 - _radius };
             const rotatedPoint2: Vector2 = rotatePoint(rotationAngle * i, startPoint2);
-            const rotatedPoint3: Vector2 = rotatePoint(rotationAngle + rotationAngle * i, startPoint2);
+            const rotatedPoint3: Vector2 = rotatePoint(rotationAngle + rotationAngle * i, startPoint3);
 
 
             point2.x = rotatedPoint2.x + centralPoint.x;
-            point2.x = rotatedPoint2.y + centralPoint.y;    
+            point2.y = rotatedPoint2.y + centralPoint.y;    
             
             point3.x = rotatedPoint3.x + centralPoint.x;
-            point3.x = rotatedPoint3.y + centralPoint.y;      
+            point3.y = rotatedPoint3.y + centralPoint.y;
+            
+            drawtriangle(_crc2,centralPoint,point2,point3,);
         }
 
         function rotatePoint(_angle: number, _point: Vector2,): Vector2 {
@@ -56,7 +58,19 @@ namespace TrianglePie {
         }
 
     }
-    // christians code and finishe circle of triangles
+
+    function drawtriangle(_crc2:CanvasRenderingContext2D,_centralpoint:Vector2,_point2:Vector2,_point3:Vector2):void{
+
+        _crc2.beginPath();
+        _crc2.moveTo(_centralpoint.x,_centralpoint.y);
+        _crc2.lineTo(_point2.x,_point2.y);
+        _crc2.lineTo(_point3.x,_point3.y);
+        _crc2.closePath()
+        _crc2.stroke();
+
+
+    }
+    
 
 
 }
